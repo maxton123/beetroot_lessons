c417c96fa47de03fc0a4fbb7e2bc9ecec231da22
Max Kapustiuk <mkapustiuk@gmail.com>
2021-09-10 14:11:24 +0300

    add in url_parser.php short and long options

--- 06-PHP-variables/url_parser.php
+++ 06-PHP-variables/url_parser.php
@@ -1,11 +1,26 @@
 <?php
+
+$url=$argv[1];
+
+/*если через short_options:
+$options= getopt("u:");
+$url=$options['u'];*/
+
+/*если через long_options:
+$options= getopt("",array ("url:"));
+$url=$options['url'];*/
+
 echo "Array of our url:".PHP_EOL;
-var_export(parse_url($argv[1]));
+var_export(parse_url($url));
 echo PHP_EOL.PHP_EOL;
 
-$query_str = parse_url($argv[1], PHP_URL_QUERY);
+$query_str = parse_url($url, PHP_URL_QUERY);
 parse_str($query_str, $query_params);
 echo "Our params:".PHP_EOL;
 var_export($query_params);
 echo PHP_EOL;
+
+
+
+
 
