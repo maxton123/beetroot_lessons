7467a2ff13034175cbbbe5fc07e10e084998f989
Max Kapustiuk <mkapustiuk@gmail.com>
2021-09-12 21:49:15 +0300

    add 09 lesson, check_lang

---
+++ 09-making-decisions/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$users = [
+    2 => ['name' => 'Cavan Dotson', 'email' => 'cavan@gmail.com', 'lang' => 'ua'],
+    3 => ['name' => 'Khadija Sutherland', 'email' => 'khadija@gmail.com', 'lang' => 'ru'],
+    5 => ['name' => 'Iyla Pacheco', 'email' => 'iyla@gmail.com', 'lang' => 'en'],
+    10 => ['name' => 'Mahir Willis', 'email' => 'mahir@gmail.com', 'lang' => 'fr'],
+    11 => ['name' => 'Summer Mcdougall', 'email' => 'summer@gmail.com', 'lang' => 'de'],
+    132 => ['name' => 'Ahmed Pollard', 'email' => 'ahmed@gmail.com', 'lang' => 'ua'],
+];
+
+$lang = [
+    'ua' => 'Привіт Козак',
+    'ru' => 'Привет Боярин)',
+    'en' => 'Hello',
+    'fr' => 'Bonjur',
+    'de' => 'Hallo',
+];
+
+$maxId = reset($users);
+$minId = end($users);
+
+
+if ($minId['lang'] === $maxId['lang']) {
+    echo $lang[$minId['lang']];
+    echo PHP_EOL;
+} else {
+    echo $lang[$minId['lang']];
+    echo PHP_EOL;
+    echo $lang[$maxId['lang']];
+    echo PHP_EOL;
+}
