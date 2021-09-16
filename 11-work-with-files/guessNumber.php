c5e4b0da3dc6fd47cd55567baf3a735a4fd7c8c0
Max Kapustiuk <mkapustiuk@gmail.com>
2021-09-16 12:39:02 +0300

    edit guessNumber.php with psr-12

--- 11-work-with-files/guessNumber.php
+++ 11-work-with-files/guessNumber.php
@@ -1,13 +1,13 @@
 <?php
-$number=isset($_POST['number']) ? $_POST['number'] : rand(0,100);
-if (isset($_POST['answer'])){
-    if ($_POST['answer']>$number){
+$number = isset($_POST['number']) ? $_POST['number'] : rand(0, 100);
+if (isset($_POST['answer'])) {
+    if ($_POST['answer'] > $number) {
         echo "загаданное число меньше введеного";
-    } elseif ($_POST['answer']<$number){
+    } elseif ($_POST['answer'] < $number) {
         echo "загаданное число больше введеного";
     } else {
         echo "вы угадали!! Было загаданно число $number. Можно пробовать еще раз)";
-        $number=rand(0,100);
+        $number = rand(0, 100);
     }
 }
 ?>
@@ -19,10 +19,10 @@ if (isset($_POST['answer'])){
 <body>
 <div id="app">
     <form action="" method="post">
-
         <p><b>Введите вариант ответа:</b><br>
             <input type="text" value="" name="answer">
-            <input type="hidden" value="<?php echo $number;?>" name="number">
+            <input type="hidden" value="<?php
+            echo $number; ?>" name="number">
         <p><input type="submit"></p>
     </form>
 </div>
